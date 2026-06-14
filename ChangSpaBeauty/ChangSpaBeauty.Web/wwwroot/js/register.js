d84f66339e2d9729fa890d5a616a25bd13d31cc1
schoolpk <truongpk2k5@gmail.com>
2026-06-14 23:57:37 +0700

    chore: add login.js and register.js Link to login.html and register.html

---
+++ ChangSpaBeauty/ChangSpaBeauty.Web/wwwroot/js/register.js
@@ -0,0 +1,11 @@
+﻿function togglePassword(inputId, iconId) {
+    const input = document.getElementById(inputId);
+    const icon = document.getElementById(iconId);
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.replace('fa-eye', 'fa-eye-slash');
+    } else {
+        input.type = 'password';
+        icon.classList.replace('fa-eye-slash', 'fa-eye');
+    }
+}
